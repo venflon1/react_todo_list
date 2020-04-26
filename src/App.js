44818c92645475fd7149fe5dd7bc96764486a957
Roberto <roberto.amato.11.93@gmail.com>
2020-04-26 14:42:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+    return (
+        
+              <div>
+                Home
+              </div>
+    );
+  }
+}
+
+export default App;
